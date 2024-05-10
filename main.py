98b4ab5885aaf59a1be963f60c8854b5f4c76423
unknown <ramirezyhonny0@gmail.com>
2024-05-10 13:25:37 -0300

    actualizacionHerencia

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash,send_from_directory
 from flask_ckeditor import CKEditor
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField,PasswordField
@@ -8,15 +8,21 @@ from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg','gif'}
 
 app = Flask(__name__)
 #CONFIGURACION A LA BASE DE DATOS
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12344567890'
+app.config['MYSQL_PASSWORD'] = 'chinoramirez'
 app.config['MYSQL_DB'] = 'proyecto_noticias'
 
 app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ckeditor = CKEditor(app)
 mysql=MySQL(app)
 
@@ -31,19 +37,13 @@ class PostForm(FlaskForm):
     contenido = CKEditorField('Contenido', validators=[DataRequired()])
     submit = SubmitField('Publicar')
 
-
 @app.route('/')
-def inicio():
-    cursor = mysql.connection.cursor()
-    sql = "SELECT * FROM noticias"
-    cursor.execute(sql)
-    noticias = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', noticias = noticias)
-
-@app.route('/cargar', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')  # Renderizar 'home.html'
+@app.route('/index', methods=['GET', 'POST'])
 def root():
     categorias = listanoticias()
+    titulos=listatitulos()
 
     form = PostForm()
     form.categoria.choices = [(c[0], c[1]) for c in categorias]  
@@ -64,9 +64,50 @@ def root():
 
 
         return redirect(url_for('root'))
-    return render_template('index.html', form=form, categorias=categorias)
+    return render_template('index.html', form=form, categorias=categorias, titulos=titulos)
 
 
+#CONFIGURACION PARA MANEJAR LAS CARGAS DE ARCHIVOS DESDE CKEDITOR
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print(request.files)  # Esto te mostrará qué archivos están llegando
+    if 'upload' not in request.files:
+        return jsonify({"uploaded": 0, "error": {"message": "No file part"}}), 400
+    file = request.files['upload']
+    if file.filename == '':
+        return jsonify({"uploaded": 0, "error": {"message": "No selected file"}}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return jsonify({
+            "uploaded": 1,
+            "fileName": filename,
+            "url": url_for('uploaded_file', filename=filename, _external=True)
+        })
+    return jsonify({"uploaded": 0, "error": {"message": "Invalid file"}}), 400
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/noticia/<int:id>')
+def noticia(id):
+    noticia = obtener_noticia_por_id(id)
+    return render_template('noticias_detalle.html', noticia=noticia)
+
+def obtener_noticia_por_id(id):
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM noticias WHERE idnoticias = %s"
+    cursor.execute(query, (id,))
+    noticia = cursor.fetchone()
+    cursor.close()
+    return noticia
 #CONSULTA A LA BASE DE DATOS PARA LAS CATEGORIAS
 def listanoticias():
     cursor = mysql.connection.cursor()
@@ -74,6 +115,14 @@ def listanoticias():
     cursor.execute(sql)
     categorias = cursor.fetchall()
     return(categorias)
+
+def listatitulos():
+    cursor = mysql.connection.cursor()
+    titulos = "Select * from noticias"
+    cursor.execute(titulos)
+    titulos = cursor.fetchall()
+    cursor.close()
+    return (titulos)
 
 #SECCIÓN DE LOGIN Y REGISTRO
 # Configuración de Flask-Login
@@ -154,4 +203,4 @@ def login():
     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
